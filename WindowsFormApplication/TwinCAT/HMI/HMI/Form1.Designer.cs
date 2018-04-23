84c90a82d380a241af8085606c7c336983419af3
nhamtung <nhamtung125@gmail.com>
2018-04-23 22:55:20 +0700

    TwinCAT HMI

---
+++ WindowsFormApplication/TwinCAT/HMI/HMI/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxNetID = new System.Windows.Forms.TextBox();
+            this.btnGetValue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(94, 26);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxValue.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(32, 71);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 1;
+            this.textBoxPort.Text = "801";
+            // 
+            // textBoxNetID
+            // 
+            this.textBoxNetID.Location = new System.Drawing.Point(155, 71);
+            this.textBoxNetID.Name = "textBoxNetID";
+            this.textBoxNetID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNetID.TabIndex = 2;
+            // 
+            // btnGetValue
+            // 
+            this.btnGetValue.Location = new System.Drawing.Point(105, 113);
+            this.btnGetValue.Name = "btnGetValue";
+            this.btnGetValue.Size = new System.Drawing.Size(75, 23);
+            this.btnGetValue.TabIndex = 3;
+            this.btnGetValue.Text = "Get Value";
+            this.btnGetValue.UseVisualStyleBackColor = true;
+            this.btnGetValue.Click += new System.EventHandler(this.btnGetValue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGetValue);
+            this.Controls.Add(this.textBoxNetID);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxNetID;
+        private System.Windows.Forms.Button btnGetValue;
+    }
+}
+
